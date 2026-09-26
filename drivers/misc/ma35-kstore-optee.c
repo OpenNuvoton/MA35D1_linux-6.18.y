6c6931e636789033359a37b6350a542f21cb03cc
Jacky Huang <ychuang3@nuvoton.com>
2026-09-26 16:40:46 +0800

    misc: ma35-kstore-optee: add OTP programming support
    Validate OTP read requests and pass shared-memory buffer
    sizes to the PTA in bytes.
    
    Handle NU_KS_IOCTL_OTP_WRITE by validating single-bit
    program requests for the FIP and Linux counter ranges
    and invoking PTA_CMD_OTP_PROGRAM.
    
    Change-Id: I46baddaf6158650371b6be4b435070b388c68b5e

--- drivers/misc/ma35-kstore-optee.c
+++ drivers/misc/ma35-kstore-optee.c
@@ -156,7 +156,7 @@ static uint16_t au8OTPCntTbl[7] = {4, 6, 6, 7, 8, 8, 8};
  * PTA_CMD_OTP_READ - Read OTP
  *
  * param[0] (in value) - value.a: OTP address
- * param[1] (inout memref) - memref.size: word count of OTP key
+ * param[1] (inout memref) - memref.size: OTP buffer size in bytes
  *                           memref.buffer: key buffer
  * param[2] unused
  * param[3] unused
@@ -167,6 +167,7 @@ static uint16_t au8OTPCntTbl[7] = {4, 6, 6, 7, 8, 8, 8};
  * TEE_ERROR_OTP_FAIL - read OTP failed
  */
 #define PTA_CMD_OTP_READ		0x12
+#define PTA_CMD_OTP_PROGRAM		0x13
 
 #define KS_DATA_SHM_SZ			1024
 
@@ -457,6 +458,12 @@ static int optee_ks_read_otp(struct optee_ks_private *ks_priv, void __user *arg)
 	ret = copy_from_user(&r_args, arg, sizeof(r_args));
 	if (ret)
 		return -EFAULT;
+	if (r_args.word_cnt <= 0 ||
+	    r_args.word_cnt > (int)ARRAY_SIZE(r_args.key) ||
+	    r_args.key_idx < 0x100 || r_args.key_idx >= 0x1D0 ||
+	    (r_args.key_idx & 3) ||
+	    r_args.word_cnt > (0x1D0 - r_args.key_idx) / 4)
+		return -EINVAL;
 
 	memset(&inv_arg, 0, sizeof(inv_arg));
 	memset(&param, 0, sizeof(param));
@@ -472,7 +479,7 @@ static int optee_ks_read_otp(struct optee_ks_private *ks_priv, void __user *arg)
 
 	param[0].u.value.a = r_args.key_idx;
 	param[1].u.memref.shm = ks_priv->ks_shm_pool;
-	param[1].u.memref.size = r_args.word_cnt;
+	param[1].u.memref.size = r_args.word_cnt * sizeof(u32);
 	param[1].u.memref.shm_offs = 0;
 
 	ret = tee_client_invoke_func(ks_priv->ctx, &inv_arg, param);
@@ -485,6 +492,45 @@ static int optee_ks_read_otp(struct optee_ks_private *ks_priv, void __user *arg)
 	memcpy((u8 *)r_args.key, ks_priv->va_shm, r_args.word_cnt * 4);
 
 	return copy_to_user(arg, &r_args, sizeof(r_args));
+}
+
+static int optee_ks_program_otp(struct optee_ks_private *ks_priv,
+				void __user *arg)
+{
+	struct ks_read_args p_args;
+	struct tee_ioctl_invoke_arg inv_arg;
+	struct tee_param param[4];
+	u32 bit;
+	int ret;
+
+	if (copy_from_user(&p_args, arg, sizeof(p_args)))
+		return -EFAULT;
+
+	bit = p_args.key[0];
+	if (p_args.word_cnt != 1 || p_args.key_idx < 0 ||
+	    (p_args.key_idx & 3) || !bit || (bit & (bit - 1)) ||
+	    !((p_args.key_idx >= 0x120 && p_args.key_idx <= 0x148) ||
+	      (p_args.key_idx >= 0x1A4 && p_args.key_idx <= 0x1CC)))
+		return -EINVAL;
+
+	memset(&inv_arg, 0, sizeof(inv_arg));
+	memset(&param, 0, sizeof(param));
+
+	inv_arg.func = PTA_CMD_OTP_PROGRAM;
+	inv_arg.session = ks_priv->session_id;
+	inv_arg.num_params = 4;
+	param[0].attr = TEE_IOCTL_PARAM_ATTR_TYPE_VALUE_INPUT;
+	param[0].u.value.a = p_args.key_idx;
+	param[0].u.value.b = bit;
+
+	ret = tee_client_invoke_func(ks_priv->ctx, &inv_arg, param);
+	if (ret < 0 || inv_arg.ret) {
+		dev_err(ks_priv->dev, "PTA_CMD_OTP_PROGRAM invoke err: %x\n",
+			inv_arg.ret);
+		return -EIO;
+	}
+
+	return 0;
 }
 
 static int optee_ks_dev_open(struct inode *iptr, struct file *fptr)
@@ -561,6 +607,10 @@ static long optee_ks_dev_ioctl(struct file *fptr, unsigned int cmd,
 
 	case NU_KS_IOCTL_OTP_READ:
 		ret = optee_ks_read_otp(ks_priv, argp);
+		break;
+
+	case NU_KS_IOCTL_OTP_WRITE:
+		ret = optee_ks_program_otp(ks_priv, argp);
 		break;
 
 	default:
